f58e9165f0871f3b55c7e65dd835cabc4c1d34f7
brenda <brendamedina03@ciencias.unam.mx>
2023-05-03 20:51:08 -0500

    nuevo

--- Casilla.java
+++ Casilla.java
@@ -34,6 +34,54 @@ public class Casilla {
       this.visitada = false;
   }
 
+  public boolean Arriba() {
+      return arriba;
+  }
+
+  public void setArriba(boolean arriba) {
+      this.arriba = arriba;
+  }
+
+  public boolean Abajo() {
+      return abajo;
+  }
+
+  public void setAbajo(boolean abajo) {
+      this.abajo = abajo;
+  }
+
+  public boolean Derecha() {
+      return derecha;
+  }
+
+  public void setDerecha(boolean derecha) {
+      this.derecha = derecha;
+  }
+
+  public boolean Izquierda() {
+      return izquierda;
+  }
+
+  public void setIzquierda(boolean izquierda) {
+      this.izquierda = izquierda;
+  }
+
+  public int getX() {
+      return x;
+  }
+
+  public void setX(int x) {
+      this.x = x;
+  }
+
+  public int getY() {
+      return y;
+  }
+
+  public void setY(int y) {
+      this.y = y;
+  }
+
   public boolean noHayVecinosSinVisitar() {
       Lista<Casilla> vecinos = obtenerVecinos();
       boolean todosVisitados = true;
@@ -75,6 +123,132 @@ public class Casilla {
       return new Casilla();
   }
 
+  public Casilla obtenerVecinoDisponible() {
+      Lista<Casilla> vecinos = obtenerVecinos();
+      Random r = new Random();
+      int rango = vecinos.longitud();
+      do {
+          int posicion = r.nextInt(rango);
+          Casilla vecino = vecinos.getElemento(posicion);
+          if (!vecino.fueVisitado() && hayPaso(vecino)) {
+              return vecino;
+          } else {
+              rango--;
+              vecinos.eliminar(vecino);
+          }
+      } while (!vecinos.esVacia());
+      return new Casilla();
+  }
+
+  public boolean hayPaso(Casilla vecino) {
+
+      switch (obtenerPuntoCardinal(vecino)) {
+          case ARRIBA:
+              return arriba;
+          case ABAJO:
+              return abajo;
+
+          case DERECHA:
+              return derecha;
+          case IZQUIERDA:
+              return izquierda;
+          default:
+              return false;
+      }
+
+  }
+
+  public Lista<Casilla> obtenerVecinos() {
+      Lista<Casilla> vecinos = new Lista<Casilla>();
+
+      // Se añaden las coordenadas así para poder recorrer los vecinos más fácilmente
+      int[][] vecinosCoordenadas = { { y, x + 1 }, { y, x - 1 }, { y + 1, x }, { y - 1, x } };
+      for (int i = 0; i < vecinosCoordenadas.length; i++) {
+          int x = vecinosCoordenadas[i][1];
+          int y = vecinosCoordenadas[i][0];
+
+          try {
+              Casilla casilla = laberinto.obtenerCasilla(x, y);
+              vecinos.agregar(casilla);
+          } catch (IllegalArgumentException e) {
+              continue;
+          }
+      }
+      return vecinos;
+  }
+
+  public boolean fueVisitado() {
+      return visitada;
+  }
+
+  public void visitar() {
+      this.visitada = true;
+  }
+
+  public void desmarcarVisitada() {
+      this.visitada = false;
+  }
+
+  public void visitar(Casilla casilla) {
+      Lista<Casilla> vecinos = obtenerVecinos();
+      if (vecinos.contiene(casilla) && !casilla.fueVisitado()) {
+          switch (obtenerPuntoCardinal(casilla)) {
+              case ARRIBA:
+                  arriba = casilla.abajo = casilla.visitada = true;
+                  break;
+              case ABAJO:
+                  abajo = casilla.arriba = casilla.visitada = true;
+                  break;
+              case DERECHA:
+                  derecha = casilla.izquierda = casilla.visitada = true;
+                  break;
+              case IZQUIERDA:
+                  izquierda = casilla.derecha = casilla.visitada = true;
+                  break;
+              default:
+                  break;
+          }
+      }
+  }
+
+  private PuntoCardinal obtenerPuntoCardinal(Casilla casilla) {
+      if (casilla.x - x == 1) {
+          return PuntoCardinal.DERECHA;
+      } else if (casilla.x - x == -1) {
+          return PuntoCardinal.IZQUIERDA;
+      } else if (casilla.y - y == 1) {
+          return PuntoCardinal.ARRIBA;
+      } else {
+          return PuntoCardinal.ABAJO;
+      }
+  }
+
+
+
+  @Override
+  public String toString() {
+      return "Casilla [este=" + este + ", norte=" + norte + ", oeste=" + oeste + ", sur=" + sur + ", visitada="
+              + visitada + ", x=" + x + ", y=" + y + "]";
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+      if (obj == null)
+          return false;
+      if (obj instanceof Casilla) {
+          Casilla c = (Casilla) obj;
+          if (c.x == x && c.y == y) {
+              if (c.norte != norte || c.sur != sur) {
+                  return false;
+              }
+              if (c.este != este || c.oeste != oeste) {
+                  return false;
+              }
+              return true;
+          }
+      }
+      return false;
+  }
 
 
 //ultimo
--- Laberinto.java
+++ Laberinto.java
@@ -8,4 +8,126 @@ public class Laberinto{
 
   private int ancho;
 
+  public Laberinto(int alto, int ancho){
+    this.alto = alto;
+    this.ancho = ancho;
+    this.casillas = generarCasillas();
+    generarLaberinto();
+  }
+
+  private void marcarTodosSinVisitar(){
+      for (int i = 0; i < casillas.length; i++) {
+          for (int j = 0; j < casillas[i].length; j++) {
+             casillas[i][j].desmarcarVisitada();
+          }
+      }
+  }
+
+  private void generarLaberinto(){
+      Casilla casilla = obtenerCasillaAlAzar();
+      casilla.visitar();
+      Pila<Casilla> pila = new Pila<Casilla>();
+      pila.push(casilla);
+      while(!pila.esVacio()){
+          casilla = pila.peek();
+          if(casilla.noHayVecinosSinVisitar()){
+              pila.pop();
+          } else {
+              Casilla vecino = casilla.obtenerVecinoSinVisitar();
+              if(casilla.getX() < 0) continue;
+              casilla.visitar(vecino);
+              pila.push(vecino);
+          }
+      }
+
+      // Esto se hace para poder encontrar un camino de una Casilla a otra con el mismo algoritmo
+      marcarTodosSinVisitar();
+  }
+
+  public Casilla[] obtenerCamino(int x1, int y1, int x2, int y2){
+      if(x1 < 0 || x1 >= ancho || y1 < 0 || y1 >= alto){
+          throw new IllegalArgumentException();
+      }
+      if(x2 < 0 || x2 >= ancho || y2 < 0 || y2 >= alto){
+          throw new IllegalArgumentException();
+      }
+      Casilla inicio = obtenerCasilla(x1, y1);
+      Casilla fin = obtenerCasilla(x2, y2);
+
+      return obtenerCamino(inicio, fin);
+
+  }
+
+  protected Casilla[] obtenerCamino(Casilla inicio, Casilla fin){
+      Pila<Casilla> pila = new Pila<Casilla>();
+      Casilla casilla = inicio;
+      casilla.visitar();
+      pila.push(casilla);
+      while(!pila.esVacio()){
+          casilla = pila.peek();
+          if(casilla.equals(fin)) break;
+          if(casilla.noHayVecinosSinVisitarConPaso()){
+              pila.pop();
+          } else {
+              Casilla vecino = casilla.obtenerVecinoDisponible();
+              vecino.visitar();
+              pila.push(vecino);
+          }
+      }
+
+      marcarTodosSinVisitar();
+      Casilla[] camino = new Casilla[pila.getTamanio()];
+
+      // En reversa, debido al como agrega elementos la pila
+      for (int i = camino.length - 1; i >= 0; i--) {
+          camino[i] = pila.pop();
+      }
+      return camino;
+  }
+
+  private Casilla obtenerCasillaAlAzar(){
+      Random r = new Random();
+      int x = r.nextInt(ancho);
+      int y = r.nextInt(alto);
+      return casillas[y][x];
+  }
+
+  private Casilla[][] generarCasillas(){
+      Casilla[][] casillas = new Casilla[alto][ancho];
+      for (int i = 0; i < casillas.length; i++) {
+          for (int j = 0; j < casillas[i].length; j++) {
+              casillas[i][j] = new Casilla(j,i, this);
+          }
+      }
+      return casillas;
+  }
+
+  protected Casilla obtenerCasilla(int x, int y){
+      if(x < 0 || x >= ancho){
+          throw new IllegalArgumentException("El laberinto no tiene el alcance para la coordenada x: " + x);
+      }
+      if(y < 0 || y >= alto){
+          throw new IllegalArgumentException("El laberinto no tiene el alcance para la coordenada y: " + y);
+      }
+      return casillas[y][x];
+  }
+
+  protected Casilla[][] getCasillas(){
+      return casillas;
+  }
+
+  public int getAlto(){
+      return alto;
+  }
+
+  public int getAncho(){
+      return ancho;
+  }
+
+  @Override
+  public String toString() {
+      return "Laberinto [alto=" + alto + ", ancho=" + ancho + "]";
+  }
+
+
 }
